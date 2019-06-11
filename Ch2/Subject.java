48c2b0e2e306503c4283079564e4fe02c1d2ace3
hipoonia <prdp295@gmail.com>
2019-06-11 20:29:59 +0530

    Observer Patter = publisher + subscriber

--- Ch2/Subject.java
+++ Ch2/Subject.java
@@ -2,5 +2,8 @@ public interface Subject {
 
 	public void registerObserver(Observer o);
 	public void removeObserver(Observer o);
-	public void notifyObservers();
+    public void notifyObservers();
+
+	// subject interface
+
 }
